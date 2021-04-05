fa49acbfcb79ddde99029e636819a1480f507deb
Alex <Alex@DESKTOP-HQCGKG2>
2021-04-05 20:41:55 +0300

    init commit

---
+++ src/test/java/fit/knu/ua/TA/TaApplicationTests.java
@@ -0,0 +1,13 @@
+package fit.knu.ua.TA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
